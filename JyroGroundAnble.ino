65f0dd9d10ee19484d7dedba7c40b998179f4751
Asck <asck.bbc@gmail.com>
2018-07-13 15:30:01 +0900

    Update JyroGroundAnble.ino

--- JyroGroundAnble.ino
+++ JyroGroundAnble.ino
@@ -24,7 +24,6 @@ void setup() {
 
 uint8_t Data[14] = {0};
 int16_t DData[7] = {0};
-float AData[7] = {0};
 float grad[3] = {0};
 
 void loop() {    
@@ -35,8 +34,6 @@ void loop() {
     uint8_t index = 0;
     
     while(Wire.available()){
-       //Serial.print(Wire.read(),BIN);
-       //Serial.print("\t");
        Data[index++] = Wire.read();
     }
     
@@ -48,41 +45,15 @@ void loop() {
     DData[5] = (int16_t)(Data[10]<<8 | Data[11]);
     DData[6] = (int16_t)(Data[12]<<8 | Data[13]);
 
-    AData[0] = ((float)DData[0]/16383)*9.81;//単位は重力加速度[g]。例、1[g] = 9.81[m/s^2]
-    AData[1] = ((float)DData[1]/16383)*9.81;
-    AData[2] = ((float)DData[2]/16383)*9.81;
-    
     grad[0] = atan2( (double) pow(pow(DData[1], 2)+pow(DData[2], 2) ,0.5) , DData[0] )*180/PI;
     grad[1] = atan2( (double) pow(pow(DData[2], 2)+pow(DData[0], 2) ,0.5) , DData[1] )*180/PI;
     grad[2] = atan2( (double) pow(pow(DData[0], 2)+pow(DData[1], 2) ,0.5) , DData[2] )*180/PI;
-    /*
-    Serial.print(" ACC[digital]:  ");
-    Serial.print(DData[0]);    Serial.print("\t,");
-    Serial.print(DData[1]);    Serial.print("\t,");
-    Serial.print(DData[2]);    Serial.print("\t");
-    */
-    Serial.print(" ACC[g]:  ");
-    Serial.print(AData[0]);    Serial.print("\t,");
-    Serial.print(AData[1]);    Serial.print("\t,");
-    Serial.print(AData[2]);    Serial.print("\t");
-    /*
-    Serial.print("\trot[deg]:  ");
-    Serial.print(atan((float)DData[1]/DData[2])*180/PI);    Serial.print("\t,");
-    Serial.print(atan((float)DData[2]/DData[3])*180/PI);    Serial.print("\t,");
-    Serial.print(atan((float)DData[3]/DData[1])*180/PI);    Serial.print("\t");
-    */
+    
     Serial.print("\tgrad[deg]:  ");
     Serial.print(grad[0]);    Serial.print("\t,");
     Serial.print(grad[1]);    Serial.print("\t,");
     Serial.print(grad[2]);    Serial.print("\t");
     
-    Serial.print("\tGYRO_X:  ");
-    Serial.print(DData[4]);    Serial.print("\t,");
-    Serial.print(DData[5]);    Serial.print("\t,");
-    Serial.print(DData[6]);    Serial.print("\t");
-    
     Serial.print("\n");
     delay(100);
-    
-
 }
